69eceec76a299164e7e998fbf9f13af3f8ba3a34
1135428665jyk <42425263+1135428665jyk@users.noreply.github.com>
2020-05-04 10:47:12 +0800

    Create bubble_sort.java

---
+++ bubble_sort.java
@@ -0,0 +1,21 @@
+//时间复杂度为O(n2)
+public class demo {
+    public static void main(String[] args) {
+        int []arr = new int[]{1,3,2,7,3,2};
+        int temp;
+        for (int i=0;i<arr.length;i++){
+            for (int j=i+1;j<arr.length;j++) {
+                if (arr[i]>arr[j]){
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (int j:arr
+        ) {
+            System.out.print(j);
+        }
+
+    }
+}
